1769a66ac7eecd68cd8dc5d8b4d5f3b6cc1a7eaf
Anna <litfin_k@yahoo.de>
2018-11-05 21:00:14 +0100

    new push

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10).join(" ")
+  end
+
+  def score
+    # raise
+    @letters = params[:letters]
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+end
+
